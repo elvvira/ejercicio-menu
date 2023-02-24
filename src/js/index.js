1af72ae6a831cc30e5b8d09561f7a0feccb93d09
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-24 14:30:35 +0100

    fin

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
 // import catIMage from '../assets/images/cat.jpeg'
 // const img = document.createElement('img');
 // img.src = catImage;
 // document.body.append(img);
-import { sayHello } from './demo.js';
-sayHello();
+// import { sayHello } from './demo.js';
+// sayHello();
+import '../scss/styles.scss';
+
+const menu = document.getElementById('menu');
+const info = document.getElementById('info');
+const image = [...document.querySelectorAll('.menu__image')];
+const texts = document.querySelectorAll('.info__text');
+const rootStyles = document.documentElement.style;
+
+const moveBall = distance => {
+  rootStyles.setProperty('--ball-position', distance + 'px');
+};
+
+menu.addEventListener('click', e => {
+  if (e.target === menu) {
+    return;
+  }
+  moveBall(e.target.offsetLeft);
+
+  image.forEach(icon => {
+    icon.classList.remove('icon-selected');
+  });
+  e.target.classList.add('icon-selected');
+
+  texts.forEach(text => {
+    text.classList.remove('info__text--show');
+  });
+
+  texts[image.indexOf(e.target)].classList.add('info__text--show');
+});
